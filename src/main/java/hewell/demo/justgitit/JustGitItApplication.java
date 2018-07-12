6e281e3bc3d521c372a54cd5b9f106725afba22e
hewell <hewell@gmail.com>
2018-07-12 23:25:16 +0800

    init commit controller for uploading video file override max file size

---
+++ src/main/java/hewell/demo/justgitit/JustGitItApplication.java
@@ -0,0 +1,12 @@
+package hewell.demo.justgitit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JustGitItApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JustGitItApplication.class, args);
+	}
+}
---
+++ src/test/java/hewell/demo/justgitit/JustGitItApplicationTests.java
@@ -0,0 +1,16 @@
+package hewell.demo.justgitit;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JustGitItApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
